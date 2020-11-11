05747fcbbf6668e23565e6b35a086b67e0353ecb
Kirill Ivanenko <kv.ivanenko@mail.ru>
2020-11-11 16:25:29 +0300

    Сверстанный проект

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function AdultFunctionminus() {
+  document.getElementById("AdultCount").stepDown();
+}
+
+function AdultFunctionplus() {
+  document.getElementById("AdultCount").stepUp();
+}
+
+function ChildrenFunctionminus() {
+  document.getElementById("ChildrenCount").stepDown();
+}
+
+function ChildrenFunctionplus() {
+  document.getElementById("ChildrenCount").stepUp();
+}
